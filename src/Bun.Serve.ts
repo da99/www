0caa1ee3509b10ba933fc8a6f147f0e027d3a7f6
da99 <spam.i.hate@miniuni.com>
2024-05-01 15:20:28 -0500

    Development checkpoint.

--- src/Bun.Serve.ts
+++ src/Bun.Serve.ts
@@ -1,6 +1,10 @@
 
+const THE_PORT = parseInt(Bun.argv[3] || '4568');
+const STATIC_DIR = normalize(`./${Bun.argv[4] || 'build'}`);
+const CMD_ARR = Bun.argv.slice(5);
+
 Bun.serve({
-  port: 4568, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
+  port: THE_PORT, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
   async fetch(req: Request) {
     const u = new URL(req.url);
     const f = Bun.file(`build/public/${u.pathname}`);
