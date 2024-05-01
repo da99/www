401515c74d1b9cb28e5f3b91d30fa5ab4a0859a3
da99 <spam.i.hate@miniuni.com>
2024-05-01 14:42:11 -0500

    Serve files manually because Bun.file sends extra bytes randomly.

--- src/Bun.Serve.ts
+++ src/Bun.Serve.ts
@@ -1,9 +1,15 @@
 
 Bun.serve({
   port: 4568, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
-  fetch(req: Request) {
+  async fetch(req: Request) {
     const u = new URL(req.url);
-    console.log(u.pathname);
-    return new Response(Bun.file(`build/public/${u.pathname}`));
+    const f = Bun.file(`build/public/${u.pathname}`);
+    const r =  new Response(await f.arrayBuffer(), {
+      headers: {
+        "Content-Type": f.type,
+      },
+    });
+    console.log(`${u.pathname} -> ${r.headers.get('Content-type')}`);
+    return r;
   },
 });
