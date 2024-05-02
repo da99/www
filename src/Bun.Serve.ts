54a670031feabdbff9b9da95526126b83daa60c9
da99 <1551848+da99@users.noreply.github.com>
2024-05-02 05:11:32 -0500

    Workaround fix because default Bun sends too many bytes sometimes.

--- src/Bun.Serve.ts
+++ src/Bun.Serve.ts
@@ -1,18 +1,27 @@
 
+import { normalize, join } from "node:path";
+
 const THE_PORT = parseInt(Bun.argv[3] || '4568');
 const STATIC_DIR = normalize(`./${Bun.argv[4] || 'build'}`);
 const CMD_ARR = Bun.argv.slice(5);
+
+console.log(`Starting server at: ${THE_PORT}, DIR: ${STATIC_DIR}, CMD: ${CMD_ARR}`)
 
 Bun.serve({
   port: THE_PORT, // defaults to $BUN_PORT, $PORT, $NODE_PORT otherwise 3000
   async fetch(req: Request) {
     const u = new URL(req.url);
-    const f = Bun.file(`build/public/${u.pathname}`);
-    const r =  new Response(await f.arrayBuffer(), {
+    const f = Bun.file(join(STATIC_DIR, u.pathname));
+    const exists = await f.exists();
+    if (!exists)
+      return new Response(`Not found: ${u.pathname}`, { status: 404 });
+
+    const r = new Response(await f.arrayBuffer(), {
       headers: {
         "Content-Type": f.type,
       },
     });
+
     console.log(`${u.pathname} -> ${r.headers.get('Content-type')}`);
     return r;
   },
