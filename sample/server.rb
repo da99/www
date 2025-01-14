f7d6e4499e427f6b1a18d41a7e241e4bfbf3698d
da99 <spam.i.hate@miniuni.com>
2025-01-14 07:01:37 -0600

    Touchups.

--- sample/server.rb
+++ sample/server.rb
@@ -2,31 +2,59 @@
 
 require 'sinatra'
 
+PAGE_404 = File.read('./sample/404.html')
+
 # set :public_folder, __dir__
 
 before do
   cache_control 'no-cache'
 end
 
-get '/' do
-  'Put this in your pipe & smoke it!'
-end
+# get '/' do
+#   'Put this in your pipe & smoke it!'
+# end
 
 get '/*.js' do
   pi = request.path_info
   fs = File.join('./sample', pi.sub(/\.js$/, '.mts'))
   content_type 'text/javascript;charset=utf-8'
+  puts("--- Building: #{fs}")
   `bun build #{fs}`
 end
 
-get '/*' do
+get '/*.*' do
   pi = request.path_info
   case pi
-  when /\.(png|gif|ico|js|css|html)$/
+  when /\.(css|html)$/
     new_file = File.join('./sample', pi)
-    puts "--- Sending file: #{new_file}"
     send_file new_file
   else
     pass
+  end
+end
+HTML_ESCAPE = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;'
+}.freeze
+
+def html_escape(raw)
+  raw.gsub(/[<>&]/) { |str| HTML_ESCAPE[str] }
+end
+
+def page_404(req)
+  PAGE_404.gsub('{PATH}', html_escape(req.path_info))
+end
+
+not_found do
+  if request.path_info[%r{/$}]
+    new_file = File.join('./sample', request.path_info, 'index.html')
+    if File.exist?(new_file)
+      send_file new_file
+    else
+      page_404(request)
+    end
+  else
+    page_404(request)
   end
 end
